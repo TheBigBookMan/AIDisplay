983f7a876b7c2c6f3ad342e7e6359fe5ed7eaba2
Ben Smerd <bsmerd@adam.com.au>
2022-12-24 13:14:00 +1030

    init commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//* Speech to text application where user speaks a prompt for the AI image creator and it creates an image and displays it automatically, kind of like a real-time art show—- imagine if there was a big screen standing up in front of you and you spoke ehat you wanted to see and it would create it and display a few seconds later, could be acool thing to have in house
+//* Make with python
+
+//* Dont need a database, or can have a very simple MongoDB one where users can store the image
