f376f9f35a546fd0576d472429151b61e7d3a3f1
Ben Smerd <bsmerd@adam.com.au>
2023-02-03 16:23:07 +1030

    added in function comments for further use

---
+++ functions.py
@@ -0,0 +1,6 @@
+
+#  todo create the api call to the ai generator with the text prompt
+
+# todo create the function that gets the speech to text and stores into a variable 
+
+#  todo create a  text to speech prompt saying back what the user said so the user can confirm
