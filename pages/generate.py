053acfc99faabb1edaf8114c1f58ea7fd058c37c
Ben Smerd <bsmerd@adam.com.au>
2023-02-03 16:21:45 +1030

    created the layout for the generate image page

--- pages/generate.py
+++ pages/generate.py
@@ -1,2 +1,7 @@
-import streamlit
+import streamlit as st
 
+st.title("AI Art")
+st.subheader("Describe a picture you would like to see and watch it be created")
+st.info("First up, we need to get across that this is a using a very basic and free AI image generator, so the results may or may not be up the standard of what you like. However it is really cool and we do recommend getting super creative with it as some results can be amazing!")
+
+# TODO add in a select folder button so they can save the picture to there if they like it and that will also save it to the favourites
