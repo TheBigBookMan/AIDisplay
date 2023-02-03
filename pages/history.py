b60696f15239e7164af6b6abd28dbff54d6bc3ef
Ben Smerd <bsmerd@adam.com.au>
2023-02-03 16:21:57 +1030

    created the layout for the history page

---
+++ pages/history.py
@@ -0,0 +1,8 @@
+
+#  todo use a text creator to put in the names of the files and then i think also have to use the os function to get any files that the user will use so it gets their txt files with the names and then can loop through them within the computer files and display them in history
+
+import streamlit as st
+
+st.title("AI Art")
+st.subheader("History section")
+st.info("Any images you created and saved will be able to be seen here again so you can view them for later use!")
